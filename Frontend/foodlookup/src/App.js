159b09a53880e0322e42483695ebc0980a1923bc
Roy <92612927+PetarWho@users.noreply.github.com>
2024-03-02 03:57:31 +0200

    Header, Footer, API Info page

--- Frontend/foodlookup/src/App.js
+++ Frontend/foodlookup/src/App.js
@@ -11,7 +11,9 @@ function App() {
       <Header/>
        <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/api" element={<API />} />
           </Routes>
+          <div className='empty'></div>
       <Footer />
     </div>
   );
---
+++ Frontend/foodlookup/src/layouts/API/API.jsx
@@ -0,0 +1,22 @@
+import Pic from "./api.png";
+import './api.css';
+
+function API(){
+    return(
+        <>
+        <div className="api-box">
+            <p className="api-info">API is located in the <b>Backend</b> folder.<br/> 
+            Open the <b>.sln</b> file and modify the connection string located in <b>appsettings.json</b>.<br/>
+            Get your SQL Server name and change the <b>Server</b> to equal your Server name (ex. "Server=Ivan\\SQLEXPRESS").<br/>
+            Then open <b>Package Manager Console <i>(Tools{">"}Nuget Package Manager{">"}Package Manager Console)</i></b>. <br/>
+            In the Package Manager Console enter <b>update-database</b>. This will create the database on your machine.<br/>
+            Now you can start the API by using the <b>green button</b> or pressing <b>F5</b>. <br />
+            Once the API is running, you won't have any records so start by creating some <b>here</b>.
+            </p>
+            <img className="api-img" src={Pic}></img>
+        </div>
+        </>
+    );
+}
+
+export default API;
