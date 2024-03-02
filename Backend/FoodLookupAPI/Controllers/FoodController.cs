acffa627a0218df45f37ab9cfaea4d5952019b03
Roy <92612927+PetarWho@users.noreply.github.com>
2024-03-02 03:58:05 +0200

    Add food action backend

--- Backend/FoodLookupAPI/Controllers/FoodController.cs
+++ Backend/FoodLookupAPI/Controllers/FoodController.cs
@@ -1,10 +1,13 @@
 ﻿using FoodLookupAPI.Data;
 using FoodLookupAPI.Entities;
+using FoodLookupAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace FoodLookupAPI.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class FoodController : Controller
     {
         private readonly ApplicationDbContext context;
@@ -12,12 +15,41 @@ namespace FoodLookupAPI.Controllers
         {
             context = _context;
         }
-        [HttpGet]
+        [HttpGet("get")]
         public async Task<List<Food>?> GetFoods(string input)
         {
             var foods = await context.Foods.Where(x => x.Description.ToLower().Contains(input.ToLower())).ToListAsync();
 
             return foods;
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(AddFoodModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var food = new Food()
+            {
+                Description = model.Description,
+                Fat = model.Fat,
+                Kcal = model.Kcal,
+                Carbs = model.Carbs,
+                Protein = model.Protein
+            };
+            try
+            {
+                await context.AddAsync(food);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Couldn't save your food to database. Please check inputs and try again!");
+            }
+
+            return Ok(food);
         }
     }
 }
